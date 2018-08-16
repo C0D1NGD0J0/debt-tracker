d55812b619e986a9f77ba69fd7eed075ff34ac00
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-16 12:59:06 -0400

    basic bootstrap navbar added

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import  { Link } from "react-router-dom";
+
+class Navbar extends Component {
+	render() {
+		return (
+			<nav className="navbar navbar-inverse navbar-fixed-top">
+	      <div className="container">
+	        <div className="navbar-header">
+	          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span className="sr-only">Toggle navigation</span>
+	            <span className="icon-bar"></span>
+	            <span className="icon-bar"></span>
+	            <span className="icon-bar"></span>
+	          </button>
+	          <Link to="/" className="navbar-brand">Project name</Link>
+	        </div>
+	        <div id="navbar" className="collapse navbar-collapse">
+	          <ul className="nav navbar-nav">
+	            <li className="active"><Link to="/">Home</Link></li>
+	            <li><Link to="#about">About</Link></li>
+	            <li><Link to="#contact">Contact</Link></li>
+	          </ul>
+	        </div>
+	      </div>
+	    </nav>
+		);
+	}
+}
+
+
+export default Navbar;
