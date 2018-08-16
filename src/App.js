dfc37e8fb7908c1affc37d95cc2ffa481648e4b7
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-16 13:07:46 -0400

    implemented basic router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      	<Navbar />
-        <h1>Welcome There</h1>
-      </div>
+    	<Router>
+	      <div className="App">
+	      	<Navbar />
+
+	      	<div className="container">
+	        	<h1>Welcome There</h1>
+	      	</div>
+	      </div>
+	    </Router>
     );
   }
 }
