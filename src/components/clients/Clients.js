379827fbbfd4d7c49518b07367d00b226ca99731
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-17 11:57:59 -0400

    updated dashboard

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -4,59 +4,77 @@ import { compose } from "redux";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { firestoreConnect } from "react-redux-firebase";
-
-
+import Spinner from "../layouts/Spinner";
 
 class Clients extends Component {
-	render() {
-		const { clients } = this.props;
+	state = {
+		totalOwed: null
+	}
+	
+	static getDerivedStateFromProps(props, state){
+		const {clients} = props;
 
 		if(clients){
-			return(
-				<div>
-					<div className="row">
-						<div className="col-md-6">
-							<h2><i className="fas fa-users"></i> Clients{' '}</h2>	
+			const total = clients.reduce((sum, client) => {
+				return sum + parseFloat(client.balance.toString());
+			}, 0);
+
+			return {totalOwed: total}
+		}
+
+		return null;
+	}
+
+	render() {
+		const { clients } = this.props;
+		const { totalOwed } = this.state;
+
+		return(
+			<React.Fragment>
+			{ clients ?
+				(
+					<div>
+						<div className="row">
+							<div className="col-md-6">
+								<h2><i className="fas fa-users"></i> Clients{' '}</h2>	
+							</div>
+
+							<div className="col-md-6">
+								<h2 className="text-right text-danger text-uppercase">Total Owed: <span className="text-primary">${parseFloat(totalOwed).toFixed(2)}</span></h2>
+							</div>
 						</div>
 
-						<div className="col-md-6">
-
-						</div>
+						<table className="table table-striped">
+							<thead className="thead-inverse">
+								<tr>
+									<th>Name</th>
+									<th>Email</th>
+									<th>Balance</th>
+									<th />
+								</tr>
+							</thead>
+							<tbody className="table-hover">
+								{
+									clients && clients.map(client =>(
+										<tr key={client.id}>
+											<td>{client.firstName} {client.lastName}</td>
+											<td>{client.email}</td>
+											<td>${parseFloat(client.balance).toFixed(2)}</td>
+											<td>
+												<Link to={`/client/${client.id}`} className="btn btn-default btn-sm">
+													<i className="fas fa-eye"></i> Details
+												</Link>
+											</td>
+										</tr>
+									))
+								}
+							</tbody>
+						</table>
 					</div>
-
-					<table className="table table-striped">
-						<thead className="thead-inverse">
-							<tr>
-								<th>Name</th>
-								<th>Email</th>
-								<th>Balance</th>
-								<th>Payment Date</th>
-								<th />
-							</tr>
-						</thead>
-						<tbody className="table-hover">
-							{
-								clients && clients.map(client =>(
-									<tr key={client.id}>
-										<td>{client.firstName} {client.lastName}</td>
-										<td>{client.email}</td>
-										<td>${parseFloat(client.balance).toFixed(2)}</td>
-										<td>{client.paymentDate}</td>
-										<td>
-											<Link to={`/client/${client.id}`} className="btn btn-default btn-sm">
-												<i className="fas fa-eye"></i> Details
-											</Link>
-										</td>
-									</tr>
-								))
-							}
-						</tbody>
-					</table>
-				</div>
-			);
-		} else {
-			return <h1>Loading data...</h1>
-		};
+				) : <Spinner />
+			}
+			</React.Fragment>
+		)
 	}
 }
 
